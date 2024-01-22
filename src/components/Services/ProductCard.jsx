f1c143de291ffd13971aae8a28041d20d701196f
perekatypole <108346224+perekatypole@users.noreply.github.com>
2024-01-22 19:05:34 +0300

    Update ProductCard.jsx

--- src/components/Services/ProductCard.jsx
+++ src/components/Services/ProductCard.jsx
@@ -1,20 +1,18 @@
-import { React, useState } from "react";
+import React from "react";
 import Image from "react-bootstrap/Image";
 import question from "../../images/main/question.svg";
-import ProductData from "../../variables/variables.js";
-import Slider from "../Slider/Slider";
+import { productData } from "../../variables/variables.js";
+import Slider from "../Slider/Slider.jsx";
 
-export default function ProductCards(){
-    const listItems = ProductData.map((oneproduct)=>(type={ProductData[]}))};
-//     return (
-//     <div className="services-container">
-//       <Image className="question" src={question} />
-//       <div className="services-card">
-//             <div className="card-title">
-// //             <h3>{}</h3>
-// //          </div>
-//           <Slider type={} />
-//       </div>
-//       </div>
-//       )
-// };
+export default function ProductCards({item}){
+console.log(item);
+    return (
+    <div className="services-card">
+      <Image className="question" src={question} />
+        <div className="card-title">
+         <h3>{productData[item].name}</h3>
+        </div>
+          <Slider type={item} />
+      </div>
+      )
+};
